bc3d735e807f3787e31fed18649225d936152ec6
danielyoon-maker <58200952+danielyoon-maker@users.noreply.github.com>
2021-12-02 00:49:53 -0800

    cleans up code and adds comments

--- main.py
+++ main.py
@@ -3,35 +3,31 @@ import cv2
 import tensorflow as tf
 import numpy as np
 
-CATEGORIES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
+classes = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"]
 
-def prepare(filepath):
+def prepare(filepath): # image processing function, takes in webcam footage and processes it into a form the model can understand
     IMG_SIZE = 200
-    new_array = cv2.resize(filepath, (IMG_SIZE, IMG_SIZE))  # resize image to match model's expected sizing
+    new_array = cv2.resize(filepath, (IMG_SIZE, IMG_SIZE))
     return new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1) 
 
-model = tf.keras.models.load_model("model")
-
-cap = cv2.VideoCapture(0)#use 0 if using inbuilt webcam
+model = tf.keras.models.load_model("model") # loads the model 
+webcam = cv2.VideoCapture(0) #use 0 if using inbuilt webcam
 
 # Check if the webcam is opened correctly
-if not cap.isOpened():
+if not webcam.isOpened():
     raise IOError("Cannot open webcam")
 
 while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = webcam.read()
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #read webcame footage as greyscale
     
-    prediction = model.predict([prepare(gray)])
-    final = (CATEGORIES[int(np.argmax(prediction[0]))])
-    
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(frame,final,(200,100), font, 1, (0,0,0), 2, cv2.LINE_AA)
+    prediction = model.predict([prepare(image)]) 
+    prediction = (classes[int(np.argmax(prediction[0]))]) #final prediction processing --> returns an index in the order of the list in line 6
+    cv2.putText(frame,prediction,(400,400), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2, cv2.LINE_AA)
 
-    cv2.imshow('Input', frame)
+    cv2.imshow('Translator', frame)
     
-
-    c = cv2.waitKey(1)
+    c = cv2.waitKey(1) # boiler plate exit on esc code
     if c == 27: # hit esc key to stop
         break
 
